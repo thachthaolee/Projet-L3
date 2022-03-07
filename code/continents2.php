fd9378b70fed8ad4b248897263eab51cf2febab1
Emilie Clement <emilie.cl123@gmail.com>
2022-03-07 14:32:20 +0100

    test avec joris

--- code/continents2.php
+++ code/continents2.php
@@ -1,84 +1,83 @@
 <!DOCTYPE html>
-
 <html>
-<head>
-    <meta  http-equiv="Content-Type" content="text/html; charset="utf-8" />
-    <link rel="stylesheet" href="Style/continent1.css" type="text/css" media="screen" />
-    <title>Continent</title>
-</head>
-
-<body>
-<?php
-	require ('bd.php');
-	$bdd = getBD();
-
-	$id = $_GET['id_continent'];
-
-	$rep = $bdd->query("SELECT * FROM continent WHERE Id_Continent= $id");
+	<head>
+		<meta  http-equiv="Content-Type" content="text/html; charset="utf-8" />
+		<link rel="stylesheet" href="Style/style.css" type="text/css" media="screen" />
+		<title>Continents</title>
+	</head>
 	
-	while ($mat=$rep-> fetch()){	
-		echo "<h1>".$mat['Nom_Continent']."</h1></br>";
-	}
-?>
-		<center>
-			</br>
-			</br>
-			</br>
-			</br>
-			</br>
-				<p>
-				</br>
-				</br>
-				</br>
-				</br>	
-					<?php
-						echo '<img src="graphe.php?continent='.$_GET["continent"].'&annee='.$_GET["annee"].'">' // call the fonction graphe
-					?>
-				
-				</p>
-		</center>
+	<body>
+		
+		<?php 	require ('bd.php');
+				$bdd = getBD();
+
+				$continent = $_GET['id_continent'];
+
+				$rep = $bdd->query("SELECT * FROM continent WHERE Id_Continent= $continent");
+							
+				while ($mat=$rep-> fetch()){	
+					echo "<h1>".$mat['Nom_Continent']."</h1></br>";
+					}
+		?>		
 		
 		<?php
-						function getBD(){
-							$bdd = new PDO('mysql:host=localhost;dbname=HapMap;charset=utf8', 'root', 'root'); 
-							return $bdd;
-						}
-						
-						$bdd=getBD();
-
-						
+				echo '<img src="graphe.php?id_continent='.$_GET["id_continent"].'&annee='.$_GET["annee"].'">' // call the fonction graphe
+		?>
+				
+		
+		
+		
+		<?php			
+			echo '<div class="continents2">';
+			$rep = $bdd->query("SELECT Nom_Pays, Id_Pays FROM pays WHERE Id_Continent = $continent");
+			while ($ligne = $rep -> fetch()) {
+				echo "<li><a href=continents3.php?id_pays=".$ligne['Id_Pays'].">".$ligne['Nom_Pays']."</a></li>";}
+				
+				$rep -> closeCursor();
+				echo "</div>";
+			?>
+					
+		
+		<?php
+				require ('bd.php');
+				$bdd = getBD();		
+					
+					
 						$rep = $bdd -> query('SELECT AVG(avoir.valeur_score) as moyenne, score.Nom_Score as nom
 												FROM score, avoir, pays, continent, annee
 												WHERE score.Id_Score = avoir.Id_Score
 												AND avoir.Id_Pays = pays.Id_Pays
 												AND pays.Id_Continent = continent.Id_Continent
 												AND annee.Annee= avoir.annee
-												AND continent.Nom_Continent="'. $_GET["continent"] . '"
+												AND score.Id_Score != 1
+												AND continent.Id_Continent="'. $_GET["continent"] . '"
 												AND annee.Annee ="' . $_GET["annee"] . '"
 												GROUP by score.Id_Score
 												order by score.Id_Score');
 												
+						
+												
 						$score = array();
 						$moyenne = array();
 						while ($ligne = $rep ->fetch()) {
-							$score[] = $ligne['moyenne'];
-							$moyenne[] = $ligne['nom'];
-							//echo "<pa>". $ligne['moyenne']." ".$ligne['nom']."</pa>";
-							echo "<pa>".$ligne['nom']."</pa>";
-							echo "<pa>".$ligne['moyenne']."</pa>";
-						}
-
-	echo '<div class="continents2">';
-	$rep = $bdd->query("SELECT Nom_Pays, Id_Pays FROM pays WHERE Id_Continent = $id");
-	while ($ligne = $rep -> fetch()) {
-        echo "<li><a href=continents3.php?id_pays=".$ligne['Id_Pays'].">".$ligne['Nom_Pays']."</a></li>";
-    }
-
-
-   
-	$rep -> closeCursor();
-	echo "</div>";
-?>
+							array_push($score,$linge['nom']);
+							array_push($moyenne,$ligne['moyenne'];
+							}
+							
+							afficher_tab($score);
+							function afficher_tab($tab){
+								$taille = count($tab);
+								for($i = 0; $i < $taille; $i++){
+									echo $tab[$i];
+								}
+							}
+							
+							afficher_tab($moyenne);
+							
+					
+						$rep -> closeCursor();
+			?>
+					
 	
 	</body>
 </html>
