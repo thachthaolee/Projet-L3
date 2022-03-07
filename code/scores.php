bf4e5d377348c693f4b07a788f6577e0224dbb7d
joris120 <joris.meynier@gmail.com>
2022-03-07 13:24:45 +0100

    le site avec les liaisons entre les pages

--- code/scores.php
+++ code/scores.php
@@ -3,7 +3,7 @@
 <html>
 
 <head>
-    <?php include('bd_wamp.php'); $bdd = getBD();?>
+    <?php include('bd.php'); $bdd = getBD();?>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <link rel="stylesheet" href="Style/StyleHapmap.css" type="text/css" media="screen" />
     <title> Scores </title>
@@ -12,20 +12,20 @@
 <body>
 
 <header>
-    <img src = "../HapMap/image/logo.png" alt = "Logo"/> 
-    <nav>
+            <img src = "../HapMap/image/logo.png" alt = "Logo"/> 
+            <nav>
             
-        <ul>
-            <li><a href="index.php">Accueil</a></li>
-            <li><a href="#" >Continent</a></li>
-            <li><a href="comparer.php">Comparer</a></li>
-            <li><a href="#">Score</a></li>
-            <li><a href="#" >A propos</a></li>
-            <!--Rajouter la fonction rechercher-->
-        </ul>
-    </nav>
+                <ul>
+                    <li><a href="index.php">Accueil</a></li>
+                    <li><a href="#" >Continent</a></li>
+                    <li><a href="comparer.php">Comparer</a></li>
+                    <li><a href="scores.php">Score</a></li>
+                    <li><a href="apropos.html" >A propos</a></li>
+                    <!--Rajouter la fonction rechercher-->
+                </ul>
+            </nav>
 
-</header>
+        </header>
 
 <table border="1">
 
