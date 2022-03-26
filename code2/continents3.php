52a6254412dd94c01ebfb19bcb1790cbd7cc3e64
Romain Poudevigne <poudvmhsc34@gmail.com>
2022-03-26 17:21:50 +0100

    Debuggage de toutes les pages

---
+++ code2/continents3.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta  http-equiv="Content-Type" content="text/html; charset="utf-8" />
+		<link rel="stylesheet" href="Style/StyleHapMap.css" type="text/css" media="screen" />
+		<title>Continents</title>
+	</head>
+
+	
+<body>
+	
+	<header>
+            <img src = "image/logo.png" alt = "Logo"/> 
+            <nav>
+            
+                <ul>
+                    <li><a href="index.php">Home page</a></li>
+					<li><a class="ici" href="continents1.php">Continent</a></li>
+					<li><a href="comparer.php">Compare</a></li>
+					<li><a href="scores.php">Score</a></li>
+					<li><a href="apropos.html">About us</a></li>
+                    <!--Rajouter la fonction rechercher-->
+                </ul>
+            </nav>
+
+    </header>
+	<br/><br/><br/>
+	
+	<div class="aff_continent3">
+		
+		<?php 	require ('bd.php');
+				$bdd = getBD();
+
+				$pays = $_GET['id_pays'];
+
+				$rep = $bdd->query("SELECT * FROM pays WHERE Id_Pays= $pays");
+							
+				while ($mat=$rep-> fetch()){	
+					echo "<h1>".$mat['Nom_Pays']."</h1></br>";
+				}
+				$rep->closeCursor();
+		?>		<br/>
+		
+		
+
+		<?php
+				echo '<img src="graphe1.php?id_pays='.$_GET["id_pays"].'&annee='.$_GET["annee"].'">' // call the fonction graphe1 as photo
+		?>
+		
+		
+		</br>	
+		</br>
+		<?php
+			 $rep = $bdd -> query('SELECT AVG(avoir.valeur_score) as moyenne, score.Nom_Score as nom
+												FROM score, avoir, pays, continent, annee
+												WHERE score.Id_Score = avoir.Id_Score
+												AND avoir.Id_Pays = pays.Id_Pays
+												AND pays.Id_Continent = continent.Id_Continent
+												AND annee.Annee= avoir.annee
+												AND score.Id_Score != 1
+												AND pays.Id_Pays="'. $_GET["id_pays"] . '"
+												AND annee.Annee ="' . $_GET["annee"] . '"
+												GROUP by score.Id_Score
+												order by score.Id_Score');
+												
+												
+			 $score = array();
+			 $moyenne = array();
+			 while ($ligne =$rep ->fetch()){
+				 $score[]=$ligne['nom'];
+				 $moyenne[]=$ligne['moyenne'];
+				 echo '<div class="indice"><p>'.$ligne['nom']."</p>";
+				 echo '<p>'.$ligne['moyenne']."</p></div>";
+			}
+			 
+			$rep -> closeCursor();
+
+			$rep = $bdd->query("SELECT * FROM pays WHERE Id_Pays= $pays");
+							
+				while ($mat=$rep-> fetch()){	
+					echo "<br/><br/><h2><a href='comparer.php?pays1=".$mat['Nom_Pays']."&continent=1'>Compare</a></h2></br>";
+				}
+				$rep->closeCursor();
+			
+		?>
+		
+	
+			
+	</div>		
+	
+</body>
+</html>
---
+++ code2/indices.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" href="Style/StyleHapmap.css" type="text/css" media="screen" />
+    <title> Indices </title>
+    <style>
+            #ici{
+    color: #148EFF;
+    border-bottom: 2px solid #148EFF;
+}
+        </style>
+</head>
+
+<body>
+
+<header>
+            <img src = "image/logo.png" alt = "Logo"/> 
+            <nav>
+            
+                <ul>
+                    <li><a href="index.php">Home page</a></li>
+					<li><a href="continents1.php">Continent</a></li>
+					<li><a href="comparer.php">Compare</a></li>
+					<li><a id="ici" href="scores.php">Score</a></li>
+					<li><a href="apropos.html">About us</a></li>
+                    <!--Rajouter la fonction rechercher-->
+                </ul>
+            </nav>
+
+        </header>
+
+<?php
+require('bd.php');
+$PDO = getBD();
+
+$id_Score = $_GET['id_Score'];
+
+$rep = $PDO -> query("SELECT * FROM score WHERE score.id_Score={$id_Score}");
+
+$ligne = $rep->fetch();
+
+echo "<h2>".$ligne['Nom_Score']."</h2>";
+echo "<br>";
+echo "The degree in which ".$ligne['Nom_Score']." contributes in the calculation of the happiness score";
+echo "<a href='classement.php'><h3> Ranking </h3></a></li>";
+echo "<a href='carte.php'><h3> Carte </h3></a></li>";
+echo "<br>";
+
+?>
