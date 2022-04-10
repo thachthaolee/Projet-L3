6dc13e83261c270cafbd73b816d8de320f4b9c4b
joris120 <joris.meynier@gmail.com>
2022-04-10 15:41:18 +0200

    bd

---
+++ code2/bd.php
@@ -0,0 +1,11 @@
+<?php
+    function getBD(){
+        //Cette ligne de code sert à accéder à la BD si vous utilisez MAMP
+        //$bdd = new PDO('mysql:host=localhost;dbname=HapMap;charset=utf8', 'root', 'root');
+
+        //Cette ligne de code sert à accéder à la BD si vous utilisez MAMP
+        //$bdd = new PDO('mysql:host=localhost;dbname=HapMap;charset=utf8','root', '');
+        
+        return $bdd;
+    }
+?>
