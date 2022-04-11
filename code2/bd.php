245126ee4961ffa2939faf16762abfd39255425f
joris120 <joris.meynier@gmail.com>
2022-04-11 13:48:24 +0200

    :)

--- code2/bd.php
+++ code2/bd.php
@@ -3,7 +3,7 @@
         //Cette ligne de code sert à accéder à la BD si vous utilisez MAMP
         //$bdd = new PDO('mysql:host=localhost;dbname=HapMap;charset=utf8', 'root', 'root');
 
-        //Cette ligne de code sert à accéder à la BD si vous utilisez MAMP
+        //Cette ligne de code sert à accéder à la BD si vous utilisez WAMP
         //$bdd = new PDO('mysql:host=localhost;dbname=HapMap;charset=utf8','root', '');
         
         return $bdd;
